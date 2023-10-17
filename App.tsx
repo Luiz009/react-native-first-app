0360a243a291d01a760e9eab653f9b0821f41d3a
Luiz Melo <lrodriguesdemelo83@gmail.com>
2023-10-16 22:04:02 -0300

    Primeira estilização do APP

--- App.tsx
+++ App.tsx
@@ -2,9 +2,27 @@ import { Text, View } from 'react-native';
 
 export default function App(){
     return(
-        <View>
-        <Text>React</Text>
-        <Text>Native</Text>
+        <View style={{
+            flex: 1,
+            backgroundColor: '#131016',
+            padding: 24
+        }}>
+
+        <Text style= {{
+            color: '#FDFCFE',
+            fontSize: 24,
+            fontWeight: 'bold',
+            marginTop: 48
+        }}>
+            Primeira Estilização
+        </Text>
+        
+        <Text style= {{
+            color: '#6B6B6B',
+            fontSize: 16
+        }}>
+            Segunda, 16 de Outubro de 2023.
+        </Text>
         </View>
     )
 }
